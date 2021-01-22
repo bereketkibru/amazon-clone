bc7fe502f561a36bbd1e0653aadf01f108bfdc06
Bereket Kibru <bekakibru2@gmail.com>
2021-01-22 23:32:28 +0300

    subtotal functionaliy done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
 import Header from "./Header";
 import Home from './Home';
+import {BrowserRouter as Router, Switch,Route} from 'react-router-dom'
+import Checkout from './Checkout';
 function App() {
   return (
+<Router>
     <div className="App">
+    <Header />
+     <Switch>
+     <Route path="/checkout">
+<Checkout/>
+     </Route>
+     <Route path="/">
      
-      <Header />
-      <Home />
+     <Home />
+     </Route>
+     
+     </Switch>
+      
     </div>
+    </Router>
   );
 }
 
